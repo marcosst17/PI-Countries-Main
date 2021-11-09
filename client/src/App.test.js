472da1676e3e05d11d6ba568ec921c0a876aea01
Marcos <marcosstx17@gmail.com>
2021-11-09 14:07:52 -0300

    ok done

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
 import Countries from './components/Countries';
 import Landing from './components/Landing';
 import store from './redux/store';
